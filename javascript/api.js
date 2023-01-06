db347eec82324a25651a1a34a08fd37b9bae0df2
nico <varela.nicoo@gmail.com>
2023-01-06 14:29:56 -0300

    clean pages

---
+++ javascript/api.js
@@ -0,0 +1,6 @@
+// "use strict";
+
+// const gallery = document.querySelector(".gallery");
+// const feed = document.querySelector(".contenedor-gallery");
+// const next = document.querySelector("#next");
+// const prev = document.querySelector("#prev");

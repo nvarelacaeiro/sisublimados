0356567308e9ac33f2c3a16c877719f9a7de52f0
nico <varela.nicoo@gmail.com>
2023-01-11 14:52:45 -0300

    submit file

--- javascript/api.js
+++ javascript/api.js
@@ -1,6 +1,11 @@
-// "use strict";
+"use strict";
 
-// const gallery = document.querySelector(".gallery");
-// const feed = document.querySelector(".contenedor-gallery");
-// const next = document.querySelector("#next");
-// const prev = document.querySelector("#prev");
+const gallery = document.querySelector(".gallery");
+const feed = document.querySelector(".contenedor-gallery");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+const token = "868162344334871";
+
+
+// how to consume instagram api with nodejs   
